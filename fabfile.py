0ca6f857dd07a3b89a64db204e590b054285166f
wuwenjie <wwj@ubuntu.(none)>
2014-08-31 21:52:41 +0800

    edx

--- fabfile.py
+++ fabfile.py
@@ -8,10 +8,10 @@
 5.with cd(PATH):
     xxx  #切换工作目录
 6.put - 将指定文件上传到指定服务器
-7.fab xx 查看所有可用函数
+7.fab xx 查看所有可用函数 #使用pip　1.8.2版本
 '''
 
-from fabric.api import local,lcd
+from fabric.api import local,lcd,settings,prefix
 #结合sh使用，用python来获取系统数据
 
 ######settings######
@@ -46,12 +46,21 @@ def test_vim():
         local("cat myvim")
 
 
-#########edx
+#########eddef edxPaverCms(sudo_user="edxapp"):
+def _bash(command='help'):
+    return "/bin/bash -l -c 'source /edx/app/edxapp/edxapp_env &&"+command + "'"
+
 def edxPaverCms():
-    """docstring for edxPaverCms"""
-    local("sudo -H -u edxapp bash && \
-          source /edx/app/edxapp/edxapp_env &&\
-          cd /edx/app/edxapp/edx-platform &&\
-          paver update_assets cms --settings=aws")
+    #with lcd("/edx/app/edxapp/edx-platform"):
+    #    local("paver update_assets cms --settings=aws")
+    with settings(sudo_user='edxapp'),prefix("/edx/app/edxapp/edxapp_env"):
+        local("paver update_assets cms --settings=aws")
+        #local("/edx/app/edxapp/edx-platform/paver update_assets cms --settings=aws")
 
+def edxIpython():
+    """docstring for edxIpython"""
+    local("sudo -u www-data /edx/bin/python.edxapp /edx/app/edxapp/edx-platform/manage.py lms --settings aws shell")
 
+def edxLog():
+    """docstring for edxLog"""
+    
