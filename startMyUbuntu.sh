35ca6182e7cb85e6cb7036ff710a666581f2ee85
wwj718 <wuwenjie718@gmail.com>
2016-02-22 10:46:50 +0800

    Create startMyUbuntu.sh

---
+++ startMyUbuntu.sh
@@ -0,0 +1,6 @@
+cd ~
+sudo apt-get install htop fish wget
+wget https://raw.githubusercontent.com/wwj718/mytools/master/.vimrc_for_server -O ~/.vimrc
+git clone https://github.com/sjl/z-fish.git "~/z-fish"
+echo . ~/z-fish/z.fish > ~/.config/fish/config.fish
+chsh -s /usr/bin/fish
