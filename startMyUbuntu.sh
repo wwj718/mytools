59a20862646434fb84f1052e9f3fc738a7777106
wwj718 <wuwenjie718@gmail.com>
2016-02-29 14:31:35 +0800

    Update startMyUbuntu.sh

--- startMyUbuntu.sh
+++ startMyUbuntu.sh
@@ -1,8 +1,8 @@
 cd ~
-sudo apt-get install htop fish wget
 wget https://raw.githubusercontent.com/wwj718/mytools/master/.vimrc_for_server -O ~/.vimrc
 git clone https://github.com/sjl/z-fish.git "~/z-fish"
 echo . ~/z-fish/z.fish > ~/.config/fish/config.fish
 chsh -s /usr/bin/fish
 #clone myNgrok
 git clone https://git.oschina.net/wuwenjie/myNgrok.git "~/ngrok"
+sudo apt-get install htop fish wget
